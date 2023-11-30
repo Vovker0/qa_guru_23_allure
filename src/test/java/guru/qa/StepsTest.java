65a6b99d8a3e69c4391a0217700257782d800f24
Vladimir <none@none>
2023-11-29 21:22:10 -0500

    Initial commit

---
+++ src/test/java/guru/qa/StepsTest.java
@@ -0,0 +1,52 @@
+package guru.qa;
+
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.Condition.exist;
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static io.qameta.allure.Allure.step;
+
+public class StepsTest {
+
+    private static final String REPOSITORY = "eroshenkoam/allure-example";
+    private static final int ISSUE = 80;
+
+    @Test
+    public void testLambdaStep() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+
+        step("Открываем главную страницу", () -> {
+            open("https://github.com/");
+        });
+        step("Ищем репозиторий " + REPOSITORY, () -> {
+            $(".search-input").click();
+            $("#query-builder-test").setValue(REPOSITORY).submit();
+        });
+        step("Кликаем по ссылке репозитория " + REPOSITORY, () -> {
+            $(By.linkText(REPOSITORY)).click();
+        });
+        step("Открываем вкладку Issues", () -> {
+            $("#issues-tab").click();
+        });
+        step("Проверяем наличие Issue с номером " + ISSUE, () -> {
+            $(withText("#" + ISSUE)).should(exist);
+        });
+    }
+
+    @Test
+    public void testAnnotatedStep() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+        WebSteps steps = new WebSteps();
+
+        steps.openMainPage();
+        steps.searchForRepository(REPOSITORY);
+        steps.clickOnRepositoryLink(REPOSITORY);
+        steps.openIssuesTab();
+        steps.shouldSeeIssueWithNumber(ISSUE);
+    }
+}
